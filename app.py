6c6da61c8e600b3659efe2e9ddc7d5a4d40355af
Cleminem <clementekuchle@hotmail.com>
2024-09-04 23:28:54 -0600

    Ruta relativa para el archivo CSV

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('D:/Desktop/MiProyecto/repositorio_proyecto_tt/vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 
 st.header('Cuadro de Mandos de Anuncios de Coches')
 
